7542f80859180d4cb4dc5702f5d2f00db2aedc62
Adams-Ghub <bamfoadamsf@gmail.com>
2023-04-25 15:55:58 +0000

    Develop the header with styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import logo from './logo.png';
 
 const element = document.querySelector('.img-ft');
-const headerImgContainer = document.querySelector('.logo-box')
+const headerImgContainer = document.querySelector('.logo-box');
 // Add the image to our existing div.
 const myIcon = new Image();
 const headerlogo = new Image();
@@ -10,9 +10,7 @@ headerlogo.src = logo;
 myIcon.src = logo;
 
 myIcon.classList.add('logo');
-headerlogo.classList.add('header-logo')
-headerImgContainer.appendChild(headerlogo)
-
-
+headerlogo.classList.add('header-logo');
+headerImgContainer.appendChild(headerlogo);
 
 element.appendChild(myIcon);
