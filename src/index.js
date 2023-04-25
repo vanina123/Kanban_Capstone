824456394e35b949377845c422e79ea84c7af5e3
vanina123 <nanavanina834@gmail>
2023-04-25 15:23:18 +0100

    Work on the footer

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+import logo from './logo.png';
+
+const element = document.querySelector('.img-ft');
+// Add the image to our existing div.
+const myIcon = new Image();
+myIcon.src = logo;
+myIcon.classList.add('logo');
+
+element.appendChild(myIcon);
